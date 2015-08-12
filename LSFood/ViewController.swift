6cfc8541d522240d6587cf6d25d92e0d95888bf8
Joel Kopelioff <joel.kopelioff@demandmedia.com>
2015-08-12 09:37:22 -0700

    Initial Commit

---
+++ LSFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LSFood
+//
+//  Created by Joel Kopelioff on 8/12/15.
+//  Copyright (c) 2015 Demandmedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
